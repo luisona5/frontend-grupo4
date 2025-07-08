c0077af428b7a6fe958b4dbc6e1aefd98f8169d6
luisona5 <zoonajluis@gmail.com>
2025-07-08 15:24:23 -0500

    Dashboard

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,23 +1,26 @@
 import { Link, Outlet, useLocation } from 'react-router'
-
+import storeAuth from '../context/storeAuth'
+import storeProfile from '../context/storeProfile'
 
 const Dashboard = () => {
     const location = useLocation()
     const urlActual = location.pathname
+    const { clearToken } = storeAuth()
+    const{user} = storeProfile()
     return (
+        
         <div className='md:flex md:min-h-screen'>
-
             <div className='md:w-1/5 bg-gray-800 px-5 py-4'>
 
-                <h2 className='text-4xl font-black text-center text-slate-200'>SmartVET</h2>
+                <h2 className='text-4xl font-black text-center text-slate-200'>PoliSport</h2>
 
                 <img src="https://cdn-icons-png.flaticon.com/512/2138/2138508.png" alt="img-client" className="m-auto mt-8 p-1 border-2 border-slate-500 rounded-full" width={120} height={120} />
-                <p className='text-slate-400 text-center my-4 text-sm'> <span className='bg-green-600 w-3 h-3 inline-block rounded-full'></span> Bienvenido - </p>
-                <p className='text-slate-400 text-center my-4 text-sm'> Rol - </p>
+                <p className='text-slate-400 text-center my-4 text-sm'> <span className='bg-green-600 w-3 h-3 inline-block rounded-full'>
+                </span> Bienvenido - {user?.nombre}</p>
+                <p className='text-slate-400 text-center my-4 text-sm'> Rol - {user?.rol}</p>
                 <hr className="mt-5 border-slate-500" />
 
                 <ul className="mt-5">
-
                     <li className="text-center">
                         <Link to='/dashboard' className={`${urlActual === '/dashboard' ? 'text-slate-200 bg-gray-900 px-3 py-2 rounded-md text-center' : 'text-slate-600'} text-xl block mt-2 hover:text-slate-600`}>Perfil</Link>
                     </li>
@@ -40,14 +43,16 @@ const Dashboard = () => {
             <div className='flex-1 flex flex-col justify-between h-screen bg-gray-100'>
                 <div className='bg-gray-800 py-2 flex md:justify-end items-center gap-5 justify-center'>
                     <div className='text-md font-semibold text-slate-100'>
-                        Usuario - 
+                        Usuario - {user?.nombre}
                     </div>
                     <div>
                         <img src="https://cdn-icons-png.flaticon.com/512/4715/4715329.png" alt="img-client" className="border-2 border-green-600 rounded-full" width={50} height={50} />
                     </div>
                     <div>
-                        <Link to='/' className=" text-white mr-3 text-md block hover:bg-red-900 text-center
-                        bg-red-800 px-4 py-1 rounded-lg">Salir</Link>
+                        <button className=" text-white mr-3 text-md block hover:bg-red-900 text-center
+                        bg-red-800 px-4 py-1 rounded-lg"
+                        onClick={() => clearToken()}
+                        >Salir</button>
                     </div>
                 </div>
                 <div className='overflow-y-scroll p-8'>
