54bcedd6d94d721002ba364fac778ddd6f6e8ba8
luisona5 <zoonajluis@gmail.com>
2025-07-08 19:07:18 -0500

    usefetch

--- src/pages/Forgot.jsx
+++ src/pages/Forgot.jsx
@@ -1,6 +1,6 @@
 import {Link} from 'react-router'
 import {useForm} from 'react-hook-form'
-import useFetch from '../Hooks/useFetch';
+import useFetch from '../Hooks/useFetch.js';
 import { ToastContainer } from 'react-toastify';
 
 
