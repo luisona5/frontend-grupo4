554670acecf4b259599d9f089a8ce3137500dd09
luisona5 <zoonajluis@gmail.com>
2025-07-08 19:04:21 -0500

    usefetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,31 +1,26 @@
-import axios from "axios";
-import { toast } from "react-toastify";
+import axios from "axios"
+import  {toast}  from "react-toastify"
 
-function useFetch() {
-    const fetchDataBackend = async (url, data = null, method = "GET",headers = {}) => {
-        const loadingToast = toast.loading("Procesando solicitud...");
+function  useFetch(){
+    const fetchDataBackend = async (url, form=null, method='POST')=>{
         try {
-            const options = {
-            method,
-            url,
-            headers: {
-                "Content-Type": "application/json",
-                ...headers,
-            },
-            data,
+            console.log(url)
+            let respuesta
+            if(method ==='POST'){
+                respuesta = await axios.post(url, form)
+                console.log(respuesta)
+            }else if(method==='GET'){
+                respuesta = await axios.get(url)
             }
-            const response = await axios(options)
-            toast.dismiss(loadingToast); 
-            toast.success(response?.data?.msg)
-            return response?.data
+            toast.success(respuesta?.data?.msg)
+            return respuesta?.data
         } catch (error) {
-            toast.dismiss(loadingToast); 
-            console.error(error)
+            console.log(error)
             toast.error(error.response?.data?.msg)
+            const errorMsg =error.response?.data?.msg || 'Error desconocido';
+            throw new Error(errorMsg);
         }
     }
-
-    return { fetchDataBackend }
+        return {fetchDataBackend}
 }
-
-export default useFetch;
+export default useFetch
