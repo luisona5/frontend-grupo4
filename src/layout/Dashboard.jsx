12e360da2bfabe2153a8062776460f608f8dc563
luisona5 <zoonajluis@gmail.com>
2025-07-09 11:57:14 -0500

    foto

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -20,7 +20,7 @@ const Dashboard = () => {
 
                 <h2 className='text-4xl font-black text-center text-slate-200'>PoliSport</h2>
 
-                <img src="https://st3.depositphotos.com/19428878/37089/v/450/depositphotos_370895286-stock-illustration-default-avatar-profile-icon-vector.jpg" alt="img-client" className="m-auto mt-8 p-1 border-2 border-slate-500 rounded-full" width={120} height={120} />
+                <img src="https://www.esan.edu.pe/images/blog/2015/07/04/futbol-peruano-figura-principal.jpg" alt="img-client" className="m-auto mt-8 p-1 border-2 border-slate-500 rounded-full" width={120} height={120} />
                 <p className='text-slate-400 text-center my-4 text-sm'> <span className='bg-green-600 w-3 h-3 inline-block rounded-full'>
                 </span> Bienvenido -{user?.nombre} </p>
                 <p className='text-slate-400 text-center my-4 text-sm'> Rol - {user?.rol}</p>
@@ -53,7 +53,7 @@ const Dashboard = () => {
                     </div>
                     <div>
                         {/* es la foto que esta a lado de salir*/}
-                        <img src="https://st3.depositphotos.com/19428878/37089/v/450/depositphotos_370895286-stock-illustration-default-avatar-profile-icon-vector.jpg" alt="img-client" className="border-2 border-green-600 rounded-full" width={50} height={50} />
+                        <img src="https://static.vecteezy.com/system/resources/previews/001/631/580/non_2x/add-photo-icon-with-camera-vector.jpg" alt="img-client" className="border-2 border-green-600 rounded-full" width={50} height={50} />
                     </div>
                     <div>
                         <button className=" text-white mr-3 text-md block hover:bg-red-900 text-center
