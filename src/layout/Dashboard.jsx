7e642d6d248b66949f6900d138343952e719521e
luisona5 <zoonajluis@gmail.com>
2025-08-06 14:16:41 -0500

    derechos

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -67,7 +67,7 @@ const Dashboard = () => {
                     <Outlet />
                 </div>
                 <div className='bg-blue-100 h-12'>
-                    <p className='text-center  text-Gray-100 '>Todos los derechos reservados- copyright </p>
+                    <p className='text-center  text-Gray-100 '><i>Todos los derechos reservados- copyright</i> </p>
                 </div>
 
             </div>
