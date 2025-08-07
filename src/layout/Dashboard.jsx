4df36ba811273e18a5ccb1263895ee4d04e2fc32
luisona5 <zoonajluis@gmail.com>
2025-08-07 12:54:21 -0500

    actualizar

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,29 +1,35 @@
 import { Link, Outlet, useLocation } from 'react-router'
 import storeAuth from '../context/storeAuth'
 import storeProfile from '../context/storeProfile'
+import { Icons } from 'react-toastify'
+
+
 
 const Dashboard = () => {
     const location = useLocation()
     const urlActual = location.pathname
     const { clearToken } = storeAuth()
+    {/*Ahora se procede a trabajar en el componente Dashboard 
+        para poder presentar la información del store.*/}
     const{user} = storeProfile()
+    
+
 
 
     return (
+        
         <div className='md:flex md:min-h-screen'>
+            <div className='md:w-1/5 bg-blue-100 px-5 py-4'> {/*franja de izquierda*/ }
 
-            <div className='md:w-1/5 bg-gray-800 px-5 py-4'>
+                <h2 className='text-3xl font-black text-center text-slate-500'>PoliSport</h2>
 
-                <h2 className='text-4xl font-black text-center text-slate-200'>SmartVET</h2>
-
-                <img src="https://cdn-icons-png.flaticon.com/512/2138/2138508.png" alt="img-client" className="m-auto mt-8 p-1 border-2 border-slate-500 rounded-full" width={120} height={120} />
+                <img src="https://www.esan.edu.pe/images/blog/2015/07/04/futbol-peruano-figura-principal.jpg" alt="img-client" className="m-auto mt-8 p-1 border-2 border-slate-500 rounded-full" width={120} height={120} />
                 <p className='text-slate-400 text-center my-4 text-sm'> <span className='bg-green-600 w-3 h-3 inline-block rounded-full'>
-                </span> Bienvenido - {user?.nombre || user?.nombrePropietario} </p>
-                <p className='text-slate-400 text-center my-4 text-sm'> Rol - {user?.rol} </p>
+                </span> Bienvenido -{user?.nombre} </p>
+                <p className='text-slate-400 text-center my-4 text-sm'> Rol - {user?.rol}</p>
                 <hr className="mt-5 border-slate-500" />
 
-                <ul className="mt-5">
-
+                <ul className="mt-5 flex justify-center space-x-4">{/*con esto es para que los link esta de manera horizontal*/ }
                     <li className="text-center">
                         <Link to='/dashboard' className={`${urlActual === '/dashboard' ? 'text-slate-200 bg-gray-900 px-3 py-2 rounded-md text-center' : 'text-slate-600'} text-xl block mt-2 hover:text-slate-600`}>Perfil</Link>
                     </li>
@@ -44,13 +50,11 @@ const Dashboard = () => {
             </div>
 
             <div className='flex-1 flex flex-col justify-between h-screen bg-gray-100'>
-                <div className='bg-gray-800 py-2 flex md:justify-end items-center gap-5 justify-center'>
+                <div className='bg-gray-100 py-2 flex md:justify-end items-center gap-5 justify-center'>
                     <div className='text-md font-semibold text-slate-100'>
-                        Usuario - {user?.nombre || user?.nombrePropietario}
+                        Usuario - {user?.nombre}
                     </div>
-                    <div>
-                        <img src="https://cdn-icons-png.flaticon.com/512/4715/4715329.png" alt="img-client" className="border-2 border-green-600 rounded-full" width={50} height={50} />
-                    </div>
+                   
                     <div>
                         <button className=" text-white mr-3 text-md block hover:bg-red-900 text-center
                         bg-red-800 px-4 py-1 rounded-lg"
@@ -61,8 +65,8 @@ const Dashboard = () => {
                 <div className='overflow-y-scroll p-8'>
                     <Outlet />
                 </div>
-                <div className='bg-gray-800 h-12'>
-                    <p className='text-center  text-slate-100 leading-[2.9rem] underline'>Todos los derechos reservados</p>
+                <div className='bg-blue-100 h-12'>
+                    <p className='text-center  text-Gray-100 '><i>Todos los derechos reservados-copyright © </i> </p>
                 </div>
 
             </div>
