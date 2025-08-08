631bc3e36920c9140c3f30efb9e25eaa5e5b05ab
luisona5 <zoonajluis@gmail.com>
2025-08-08 04:45:55 -0500

    App

--- src/App.jsx
+++ src/App.jsx
@@ -12,28 +12,14 @@ import List from './pages/List'
 import Details from './pages/Details'
 import Create from './pages/Create'
 import Update from './pages/Update'
+
 import Chat from './pages/Chat'
 import Reset from './pages/Reset'
 import PublicRoute from './routes/PublicRoute'
 import ProtectedRoute from './routes/ProtectedRoute'
-import { useEffect } from 'react'
-import storeProfile from './context/storeProfile'
-import storeAuth from './context/storeAuth'
-
 
 
 function App() {
-
-  const { profile} = storeProfile()
-  const { token } = storeAuth()
-
-  useEffect(() => {
-    if(token){
-      profile()
-    }
-  }, [token])
-  
-  
   return (
     <>
     <BrowserRouter>
